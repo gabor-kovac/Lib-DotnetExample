6112fbbefbb18d77c116d7cd50c6817458a7b05b
gabor-kovac <gabor.kovac@ametek.com>
2025-07-21 10:24:51 +0200

    Add json deserilalization support

--- Source/LoggingComponent/Logger.cs
+++ Source/LoggingComponent/Logger.cs
@@ -1,9 +1,17 @@
-﻿namespace LoggingComponent;
+﻿using Newtonsoft.Json;
+
+namespace LoggingComponent;
 
 public static class Logger
 {
     public static void Log(string message)
     {
         Console.WriteLine($"Log: {message}");
+    }
+
+    public static void Log(object obj)
+    {
+        string json = JsonConvert.SerializeObject(obj);
+        Log(json);
     }
 }
